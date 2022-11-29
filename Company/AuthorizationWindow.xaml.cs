ebff757ad538ed0de865bd53351870e5366af11a
Александр Баскаков <basc11111@gmail.com>
2022-11-29 15:51:50 +0300

    Initial commit

---
+++ Company/AuthorizationWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Company;
+
+public partial class AuthorizationWindow : Window{
+    public AuthorizationWindow(){
+        InitializeComponent();
+    }
+
+    private void AuthBtn_OnClick(object sender, RoutedEventArgs e){
+        throw new System.NotImplementedException();
+    }
+
+    private void RegBtn_OnClick(object sender, RoutedEventArgs e){
+        throw new System.NotImplementedException();
+    }
+}
